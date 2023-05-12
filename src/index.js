4a9f9f10f89bf11205041cf783d5f3b1bd7cb734
Ricardo Berti <mr.ricardoberti@gmail.com>
2023-05-12 15:50:46 -0300

    Subindo a segunda parte do projeto
    Inclusão do CRUD do recado

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ app.listen(5555, () => console.log("Servidor rodando"));
 let usuarios = [];
 
 //Create - POST
-
 app.post("/usuarios", (request, response) => {
   const usuario = request.body;
   usuarios.push({
@@ -20,7 +19,7 @@ app.post("/usuarios", (request, response) => {
     email: usuario.email,
     senha: usuario.senha,
   });
-  response.json("ok");
+  response.json("Usuário criado com sucesso");
 });
 
 // Read - lê todos os usuários
@@ -67,6 +66,68 @@ app.delete('/usuarios/:id', (request, response) => {
   if (indiceUsuario !== -1) {
     usuarios.splice(indiceUsuario, 1);
     response.status(200).json()
+  } else {
+    response.status(401).json()
+  }
+})
+//----------------------------------------------------------------------------------------------
+
+// CRUD PARA CRIAR RECADO
+let recados = [];
+
+//Create - POST
+app.post("/usuarios/:id/recados", (request, response) => {
+  const recado = request.body;
+  recados.push({
+    id: Math.floor(Math.random() * 6584123),
+    titulo: recado.titulo,
+    descricao: recado.descricao,
+  });
+  response.json("Recado criado com sucesso");
+});
+
+// Read - lê todos os recados
+app.get("/usuarios/:id/recados", (request, response) => {
+  //para mostrar a informação ao front-end:
+  response.status(200).json(recados);
+})
+
+// Read - um recado só (route params)
+app.get('/usuarios/:id/recados/:id', (request, response)=> {
+  const id = request.params.id;
+  const recado = recados.find(recado => recado.id == id);
+  if(recado){
+    response.json(recado)
+} else {
+    response.json("Id invalido")
+}
+})
+
+// Update - edita/atualiza informações do usuário
+app.put('/usuarios/:id/recados/:id', (request, response) => {
+  const recado = request.body;
+  const id = Number(request.params.id);
+  const indiceRecado = recados.findIndex(recado => recado.id === id)
+  
+  if (indiceRecado !== -1) {
+    recados[indiceRecado] = {
+      id: id,
+      titulo: recado.titulo,
+      descricao: recado.descricao,
+    }
+    response.status(201).json("Recado editado com sucesso!");  
+  } else {
+    response.status(401).json()
+  }
+})
+
+// Delete
+app.delete('/usuarios/:id/recados/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const indiceRecado = recados.findIndex(recado => recado.id === id)
+  if (indiceRecado !== -1) {
+    recados.splice(indiceRecado, 1);
+    response.status(200).json("Recado deletado com sucesso!")
   } else {
     response.status(401).json()
   }
