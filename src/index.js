6c0745bec6d528dd1b2f87ba8859f1b92e4d80e7
Ricardo Berti <mr.ricardoberti@gmail.com>
2023-05-12 23:42:23 -0300

    Implementação da rota de login
    e exclusão das rotas de edição e exclusão de usuário.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
@@ -13,123 +14,123 @@ let usuarios = [];
 //Create - POST
 app.post("/usuarios", (request, response) => {
   const usuario = request.body;
-  usuarios.push({
-    id: Math.floor(Math.random() * 6584123),
-    nome: usuario.nome,
-    email: usuario.email,
-    senha: usuario.senha,
+  const saltRounds = 10;
+
+  bcrypt.hash(usuario.senha, saltRounds, function (err, hash) {
+    if (hash) {
+      usuarios.push({
+        id: Math.floor(Math.random() * 6584123),
+        nome: usuario.nome,
+        email: usuario.email,
+        senha: hash,
+      });
+    } else {
+      return response.status(400).json(`Ocorreu um erro ${err}`);
+    }
   });
-  response.json("Usuário criado com sucesso");
+
+  return response.json("Usuário criado com sucesso");
 });
 
 // Read - lê todos os usuários
 app.get("/usuarios", (request, response) => {
-  //para mostrar a informação ao front-end:
   response.status(200).json(usuarios);
-})
+  //para mostrar a informação ao front-end:
+});
+//----------------------------------------------------------------------------------------------
+//LOGIN
+app.post("/usuarios/login", async (request, response) => {
+    const login = request.body;
+    const email = login.email;
+    const senha = login.senha;
 
-// Read - um usuário só (route params)
-app.get('/usuarios/:id', (request, response)=> {
-  const id = request.params.id;
-  const usuario = usuarios.find(usuario => usuario.id == id);
-  if(usuario){
-    response.json(usuario)
-} else {
-    response.json("Id invalido")
-}
-})
-
-// Update - edita/atualiza informações do usuário
-app.put('/usuarios/:id', (request, response) => {
-  const usuario = request.body;
-  const id = Number(request.params.id);
-  const indiceUsuario = usuarios.findIndex(usuario => usuario.id === id)
-  
-  if (indiceUsuario !== -1) {
-    usuarios[indiceUsuario] = {
-      id: id,
-      nome: usuario.nome,
-      email: usuario.email,
-      senha: usuario.senha,
+    const usuario = usuarios.find((usuario) => usuario.email === email);
+    if (usuario) {
+      return response.status(200).json("Usuário válido");
+    } else {
+      return response.status(402).json("Por favor, digite um email válido");
     }
-    response.status(201).json(usuarios[indiceUsuario]);  
-  } else {
-    response.status(401).json()
-  }
-})
+  });
 
-
-// Delete
-app.delete('/usuarios/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const indiceUsuario = usuarios.findIndex(usuario => usuario.id === id)
-  if (indiceUsuario !== -1) {
-    usuarios.splice(indiceUsuario, 1);
-    response.status(200).json()
-  } else {
-    response.status(401).json()
-  }
-})
 //----------------------------------------------------------------------------------------------
 
 // CRUD PARA CRIAR RECADO
 let recados = [];
 
 //Create - POST
-app.post("/usuarios/:id/recados", (request, response) => {
+app.post("/usuarios/login/:id/recados", (request, response) => {
   const recado = request.body;
   recados.push({
     id: Math.floor(Math.random() * 6584123),
     titulo: recado.titulo,
     descricao: recado.descricao,
   });
-  response.json("Recado criado com sucesso");
+  return response.json("Recado criado com sucesso");
 });
 
 // Read - lê todos os recados
-app.get("/usuarios/:id/recados", (request, response) => {
+app.get("/usuarios/login/:id/recados", (request, response) => {
   //para mostrar a informação ao front-end:
-  response.status(200).json(recados);
-})
+  return response.status(200).json(recados);
+});
 
 // Read - um recado só (route params)
-app.get('/usuarios/:id/recados/:id', (request, response)=> {
-  const id = request.params.id;
-  const recado = recados.find(recado => recado.id == id);
-  if(recado){
-    response.json(recado)
-} else {
-    response.json("Id invalido")
-}
-})
+app.get(
+  "/usuarios/login/:id/recados/:id",
+  validaIdRecado,
+  (request, response) => {
+    const id = request.params.id;
+    const recado = recados.find((recado) => recado.id == id);
+    if (recado) {
+      return response.json(recado);
+    } else {
+      return response.json("Id invalido");
+    }
+  }
+);
 
 // Update - edita/atualiza informações do usuário
-app.put('/usuarios/:id/recados/:id', (request, response) => {
-  const recado = request.body;
-  const id = Number(request.params.id);
-  const indiceRecado = recados.findIndex(recado => recado.id === id)
-  
-  if (indiceRecado !== -1) {
-    recados[indiceRecado] = {
-      id: id,
-      titulo: recado.titulo,
-      descricao: recado.descricao,
+app.put(
+  "/usuarios/login/:id/recados/:id",
+  validaIdRecado,
+  (request, response) => {
+    const recado = request.body;
+    const id = Number(request.params.id);
+    const indiceRecado = recados.findIndex((recado) => recado.id === id);
+
+    if (indiceRecado !== -1) {
+      recados[indiceRecado] = {
+        id: id,
+        titulo: recado.titulo,
+        descricao: recado.descricao,
+      };
+      return response.status(201).json("Recado editado com sucesso!");
+    } else {
+      return response.status(401).json();
     }
-    response.status(201).json("Recado editado com sucesso!");  
-  } else {
-    response.status(401).json()
   }
-})
+);
 
 // Delete
-app.delete('/usuarios/:id/recados/:id', (request, response) => {
+app.delete("/usuarios/login/:id/recados/:id", (request, response) => {
   const id = Number(request.params.id);
-  const indiceRecado = recados.findIndex(recado => recado.id === id)
+  const indiceRecado = recados.findIndex((recado) => recado.id === id);
   if (indiceRecado !== -1) {
     recados.splice(indiceRecado, 1);
-    response.status(200).json("Recado deletado com sucesso!")
+    return response.status(200).json("Recado deletado com sucesso!");
   } else {
-    response.status(401).json()
+    return response.status(401).json();
   }
-})
+});
 //----------------------------------------------------------------------------------------------
+//MIDDLEWARES:
+function validaIdRecado(request, response, next) {
+  const id = request.params.id;
+  //para validar se o id existe:
+  const index = recados.findIndex((recado) => recado.id == id);
+  if (index == -1) {
+    return response.status(400).json("Por favor, passe um ID válido");
+  } else {
+    next();
+  }
+}
